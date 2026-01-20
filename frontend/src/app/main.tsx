a1fa172bd7972c41a60b908c09361a1508c4ffe9
alokgoldy <alokkrgoldy@gmail.com>
2026-01-20 23:05:36 +0530

    added main tsx file

---
+++ frontend/src/app/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { ApolloProvider } from '@apollo/client';
+import { store } from './app/store';
+import { apolloClient } from './app/apollo';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <ApolloProvider client={apolloClient}>
+        <App />
+      </ApolloProvider>
+    </Provider>
+  </React.StrictMode>
+);
