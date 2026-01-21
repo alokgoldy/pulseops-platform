9609712c1d855b3a4a4ab06a9d860549b8d3bc29
alokgoldy <alokkrgoldy@gmail.com>
2026-01-21 21:55:32 +0530

    fixed main tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { ApolloProvider } from '@apollo/client/react';
+import { store } from './app/store';
+import { client } from './app/apollo';
+import App from './App';
+import './index.css';
+import './sentry';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </Provider>
+  </React.StrictMode>
+);
