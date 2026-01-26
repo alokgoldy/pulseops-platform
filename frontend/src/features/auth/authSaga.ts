ee39bfdbf82bf458c1c1948e596edcbd45db7249
alokgoldy <alokkrgoldy@gmail.com>
2026-01-26 22:32:56 +0530

    fix: fixed explicit any in authSaga

--- frontend/src/features/auth/authSaga.ts
+++ frontend/src/features/auth/authSaga.ts
@@ -1,6 +1,7 @@
 import { call, put, takeLatest } from 'redux-saga/effects';
-import { apolloClient } from '../../app/apollo';
+import { client } from '../../app/apollo';
 import { LOGIN_MUTATION } from './auth.graphql';
+import type { User } from './auth.types';
 import {
   loginRequest,
   loginSuccess,
@@ -9,18 +10,22 @@ import {
 
 function* loginWorker(
   action: ReturnType<typeof loginRequest>
-): Generator<any, void, any> {
+): Generator<unknown, void, { data: { login: User } }> {
   try {
     const { email, password } = action.payload;
 
-    const response = yield call([apolloClient, apolloClient.mutate], {
+    const response = yield call([client, client.mutate], {
       mutation: LOGIN_MUTATION,
       variables: { email, password },
     });
 
     yield put(loginSuccess(response.data.login));
-  } catch (err: any) {
-    yield put(loginFailure(err.message || 'Login failed'));
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      yield put(loginFailure(err.message));
+    } else {
+      yield put(loginFailure('An unknown error occurred'));
+    }
   }
 }
 
