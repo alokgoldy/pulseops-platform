2437f0893cc3ee9490cf022a82d233b522c49c53
alokgoldy <alokkrgoldy@gmail.com>
2026-02-03 23:58:56 +0530

    fixed comment

--- frontend/src/app/apollo.ts
+++ frontend/src/app/apollo.ts
@@ -1,3 +1,6 @@
+// Apollo Client setup for GraphQL requests
+// Reads endpoint from VITE_GRAPHQL_URI; defaults to http://localhost:4000/graphql
+// Exports a configured ApolloClient instance with HttpLink and InMemoryCache
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const httpLink = new HttpLink({
